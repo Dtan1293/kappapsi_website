8b08668beb45372b6b5fcc7260a4da6be62e3694
Dtan1293 <Dtan1293@gmail.com>
2018-01-29 23:31:59 -0800

    figured out associated js arrays and synchronized arrays

--- webmaster/webmaster.js
+++ webmaster/webmaster.js
@@ -1,3 +1,7 @@
+var email_array = [];
+var ID_array = [];
+var count = 0;
+
 window.onload = function() {
     var config = {
     apiKey: "AIzaSyB3IgkjXuZNJUDVkgZAxQf3HUGW8EOS6fI",
@@ -11,8 +15,17 @@ window.onload = function() {
         //injectFireaseCode();
     loadMCardTable();
     //injectNewChapterCode();
-    updateChapterInfo();
-    //AddNewChapter("stanley's_chapter");
+    //updateChapterInfo();
+    //addNewChapter("stanley's_chapter", "1234@gmail.com", "/hello/world/dawg");
+  }
+
+  function syncChanges(list, ref) {
+    ref.on('child_added', function _add(snap, prevChild) {
+      var data = snap.val();
+      data.$id = snap.key(); // assumes data is always an object
+      var pos = positionAfter(list, prevChild);
+      list.splice(pos, 0, data);
+    });
   }
 
 function loadMCardTable() {
@@ -33,6 +46,14 @@ function loadMCardTable() {
         memEmail = childnode.val().email; 
         memLink = childnode.val().link;
 
+        //add it to our "hashtable"
+        //console.log(memEmail);
+        //console.log("1");
+        //email_array.push(memEmail);
+        email_array[memID] = memEmail;
+        //ID_array[count++] = memID;
+        //console.log(count);
+
         //adding data to the tabel on the page    
         //create a new row
         tr = $('<tr>');
@@ -48,29 +69,52 @@ function loadMCardTable() {
         tr.append(td);
         tbody.append(tr);
       });
+      console.log("Done loading!" + email_array);
+      for(var i in email_array) {
+          console.log(i + email_array[i])
+      }
+      //displaytest();
     });
   }
 
-function AddNewChapter() {
+function addNewChapter(chapter_name, email_address, link_string) {
     var contactsRef = firebase.database().ref('Chapter_Email_To_Database_Link');
     contactsRef.push({
-        chapter: "David_Tan",
-        email: "yellow@gmail.com", 
-        link : "Northeast_Province/Beta_Omicron/Roster"
+        chapter: chapter_name,
+        email: email_address, 
+        link : link_string
     });
 }
 
-function updateChapterInfo() {
+function updateChapterInfo(id) {
      //referencing the unique child id for updating / deleting
-    var contactsRef = firebase.database().ref('Chapter_Email_To_Database_Link/-KyyaGWp4O3YVIcwhEhb');
+    var contactsRef = firebase.database().ref('Chapter_Email_To_Database_Link/' + id);
     contactsRef.update({
         email: "Dtan1293@gmail.com"
     });
 }
 
 //this will permanently delete a chapter! Only to be used incase created by mistake!
-function removeChapterInfo() {
+function removeChapterInfoByEmail(email_address) {
+    var adaRef = firebase.database().ref('users/ada');
+    adaRef.remove()
+    .then(function() {
+        console.log("Remove succeeded.")
+    })
+    .catch(function(error) {
+        console.log("Remove failed: " + error.message)
+    });
+}
 
+function removeChapterInfoByID(id) {
+    var deletenode = firebase.database().ref('Chapter_Email_To_Database_Link/' + id);
+    deletenode.remove()
+    .then(function() {
+        console.log("Remove succeeded.")
+    })
+    .catch(function(error) {
+        console.log("Remove failed: " + error.message)
+    });
 }
 
 //declares the chapter inactive
@@ -83,4 +127,8 @@ function moveChapterInactive() {
 //this will display on the html active
 function moveChapterActive() {
 
+}
+
+function displaytest() {
+    console.log(email_array[0]);
 }
